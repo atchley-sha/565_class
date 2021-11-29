c41fabe279cd3d91a78f61a6cb763eb6945b29df
Hayden Atchley <atchley.sha@gmail.com>
2021-11-29 16:03:21 -0700

    Final project stuff

--- PROJECT/project.R
+++ PROJECT/project.R
@@ -4,33 +4,29 @@ pacman::p_load(
 )
 install_github("atchley-sha/R-packageSHA")
 library(packageSHA)
+################################################################################
 
-
-
-
+#Import SE data
 noBuild <- "data/se_classified_2040_NO-BUILD.dbf" %>% 
   read.dbf() %>% 
   as_tibble()
 
-zonal <- "data/ZONAL AT2040A.DBF" %>% 
-  read.dbf() %>% 
-  as_tibble()
-
-fullBase <- left_join(noBuild,
-          zonal,
-          by = c("Z" = "N")
-          )
-
-base <- fullBase %>% 
+#Rearrange SE data
+base <- noBuild %>% 
   select(
     -DISTRICT,
     -COUNTY,
     -ACRES,
-    -SG_NAME
+    -SG_NAME,
+    -N,
+    -(w0v0:p4v3)
   ) %>% 
   replace(is.na(.), 0) %>% 
-  relocate(c(Z, ATYPE))
+  relocate(c(Z, POP))
 
+################################################################################
+
+#Set downtown zone IDs
 DTZones <- c(88,89,90,91,92,
              93,102,117,118,119,
              126,127,128,129,130,
@@ -38,8 +34,14 @@ DTZones <- c(88,89,90,91,92,
              195,196,197,198,200,
              201)
 
-NCZones <- c(15,16,17)
+#Set neighborhood centers zone IDs
+NCZones <- c(42,66,97,148,149,
+             160,161,176,177,178,
+             179,180,181)
 
+################################################################################
+
+#Function to sample residents and jobs and move them downtown
 sample_downtown <- function(scen, frac, zones){
   
   scenDT <- scen %>% 
@@ -48,28 +50,40 @@ sample_downtown <- function(scen, frac, zones){
   scenRU <- scen %>% 
     filter(!Z %in% zones)
   
-  additions <- colSums(
-    select(scenRU,
-           !c(Z, ATYPE))
-    ) * frac
+  additions <- colSums(select(scenRU, !Z)) * frac
   
-  scenDT[,3:length(colnames(scenDT))] <- 
-    scenDT[,3:length(colnames(scenDT))] +
+  scenDT[,2:length(colnames(scenDT))] <- 
+    scenDT[,2:length(colnames(scenDT))] +
     (
       (additions / nrow(scenDT)) %>% 
         {.[col(
           scenDT %>%
-            select(3:length(colnames(scenDT))))]
+            select(2:length(colnames(scenDT))))]
         }
     )
   
-  scenRU[,3:length(colnames(scenRU))] <- 
-    scenRU[,3:length(colnames(scenRU))] * (1-frac)
+  scenRU[,2:length(colnames(scenRU))] <- 
+    scenRU[,2:length(colnames(scenRU))] * (1-frac)
   
-  rbind(scenDT, scenRU) %>% 
-    arrange(Z)
+  scen <- rbind(scenDT, scenRU) %>% 
+    arrange(Z) %>% 
+    mutate(across(POP:SG_COL,
+           as.integer))
+  tibble(
+    Z = noBuild$Z,
+    DISTRICT = noBuild$DISTRICT,
+    COUNTY = noBuild$COUNTY,
+    ACRES = noBuild$ACRES,
+    scen %>% 
+      select(-Z),
+    SG_NAME = noBuild$SG_NAME,
+    N = noBuild$N
+  )
 }
 
+
+#Function to sample residents and jobs and move them to neighborhood centers
+#(keeps the downtown area unchanged)
 sample_nc <- function(scen, frac, zonesDT, zonesNC){
   
   scenDT <- scen %>% 
@@ -81,33 +95,75 @@ sample_nc <- function(scen, frac, zonesDT, zonesNC){
   scenRU <- scen %>% 
     filter(!Z %in% zonesDT, !Z %in% zonesNC)
   
-  additions <- colSums(
-    select(scenRU,
-           !c(Z, ATYPE))
-  ) * frac
+  additions <- colSums(select(scenRU, !Z)) * frac
   
-  scenNC[,3:length(colnames(scenNC))] <- 
-    scenNC[,3:length(colnames(scenNC))] +
+  scenNC[,2:length(colnames(scenNC))] <- 
+    scenNC[,2:length(colnames(scenNC))] +
     (
       (additions / nrow(scenNC)) %>% 
         {.[col(
           scenNC %>%
-            select(3:length(colnames(scenNC))))]
+            select(2:length(colnames(scenNC))))]
         }
     )
   
-  scenRU[,3:length(colnames(scenRU))] <- 
-    scenRU[,3:length(colnames(scenRU))] * (1-frac)
+  scenRU[,2:length(colnames(scenRU))] <- 
+    scenRU[,2:length(colnames(scenRU))] * (1-frac)
   
-  rbind(scenDT, scenNC, scenRU) %>% 
-    arrange(Z)
+  scen <- rbind(scenDT, scenNC, scenRU) %>% 
+    arrange(Z) %>% 
+    mutate(across(POP:SG_COL,
+                  as.integer))
+  
+  tibble(
+    Z = noBuild$Z,
+    DISTRICT = noBuild$DISTRICT,
+    COUNTY = noBuild$COUNTY,
+    ACRES = noBuild$ACRES,
+    scen %>% 
+      select(-Z),
+    SG_NAME = noBuild$SG_NAME,
+    N = noBuild$N
+  )
 }
 
-DTscen <- sample_downtown(base, 0.3, DTZones) %>% 
-  as.data.frame()
+################################################################################
 
-NCscen <- sample_nc(base, 0.3, DTZones, NCZones) %>% 
-  as.data.frame()
+#Write .dbf's of sampled downtown
+sample_downtown(base, 0.3, DTZones) %>% 
+  as.data.frame() %>% 
+  write.dbf("data/se_downtown30.dbf")
 
-write.dbf(DTscen, "data/se_downtown.dbf")
-write.dbf(NCscen, "data/se_neighborhood.dbf")
+sample_downtown(base, 1.0, DTZones) %>% 
+  as.data.frame() %>% 
+  write.dbf("data/se_downtown100.dbf")
+
+
+#Write .dbf's of sampled neighborhood centers
+sample_nc(base, 0.3, DTZones, NCZones) %>% 
+  as.data.frame() %>% 
+  write.dbf("data/se_neighborhood30.dbf")
+
+sample_nc(base, 1.0, DTZones, NCZones) %>% 
+  as.data.frame() %>% 
+  write.dbf("data/se_neighborhood100.dbf")
+
+################################################################################
+
+#Check output colsums match input colsums
+sums0 <- colSums(base)
+
+col_select <- function(df, base){
+  df %>% 
+    select(colnames(base))
+}
+
+sums1 <- colSums(col_select(sample_downtown(base, 0.3, DTZones), base))
+sums2 <- colSums(col_select(sample_downtown(base, 1.0, DTZones), base))
+sumsA <- colSums(col_select(sample_nc(base, 0.3, DTZones, NCZones), base))
+sumsB <- colSums(col_select(sample_nc(base, 1.0, DTZones, NCZones), base))
+
+sums <- c(sums1, sums2, sumsA, sumsB)
+
+#Check errors
+rmserr(rep.int(sums0, 4), sums)
